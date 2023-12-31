8c53450ff9f684e1ec1c7a2b48cc5c29d6d96ec1
harodler <haj.rodler@gmail.com>
2023-12-31 17:50:53 +0100

    chore: initial kanban board from tutorial

---
+++ src/components/common/KanbanDropZone.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { TRANSFER_DATA } from '@/types'
+
+const emit = defineEmits<{
+  (e: 'dropData', payload: TRANSFER_DATA): void
+}>()
+
+function onDrop(e: DragEvent) {
+  if (e.dataTransfer) {
+    let payload = JSON.parse(e.dataTransfer.getData('payload'))
+    emit('dropData', payload)
+  }
+}
+</script>
+
+<template>
+  <div @drop.stop="onDrop" @dragover.prevent @dragenter.prevent>
+    <slot />
+  </div>
+</template>
