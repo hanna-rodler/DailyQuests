576d5d63209f351fb3ec5c9e409e080e139b7a75
RawanGomaa <s2200458025@students.fh-hagenberg.at>
2024-01-23 20:51:23 +0100

    Automated test on navebar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,22 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    jest: true, // Add this line
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    // 'plugin:jest/recommended' // Uncomment this if you have installed eslint-plugin-jest
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  plugins: [
+    'jest'
+  ],
 }
